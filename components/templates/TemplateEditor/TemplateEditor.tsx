53384c32f7435a343d7c0335c9ef0a62a153d811
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2024-10-10 04:56:30 +0100

    feat : implement template screens UI

--- components/templates/TemplateEditor/TemplateEditor.tsx
+++ components/templates/TemplateEditor/TemplateEditor.tsx
@@ -15,8 +15,22 @@ const TemplateEditor: React.FC<ITemplateEditorProps> = ({
   return (
     <div className="w-full">
       <Editor
+        toolbar={{
+          fontFamily: {
+            options: [
+              "Arial",
+              "Georgia",
+              "Impact",
+              "Tahoma",
+              "Times New Roman",
+              "Verdana",
+            ],
+          },
+          fontSize: {
+            options: [8, 9, 10, 11, 12, 14, 16, 18, 24, 30, 36, 48, 60, 72, 96],
+          },
+        }}
         editorState={editorState}
-        wrapperClassName="demo-wrapper"
         editorClassName="!h-[300px] border border-input mt-8 px-3 py-2 bg-white"
         onEditorStateChange={onEditorStateChange}
       />
