6a0b29f4d53fa4408f5f1531803e3046d72f5c6c
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2024-10-11 15:51:16 +0100

    chore : prefill login details

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -7,17 +7,23 @@ import { Label } from "@/components/ui/label";
 export default function Login({ searchParams }: { searchParams: Message }) {
   return (
     <form className="flex-1 flex justify-center flex-col  w-full">
-      <h1 className="text-2xl font-medium">Sign in</h1>
+      <h1 className="text-2xl font-medium">
+        Sign in (use the credentials in inputs placeholder)
+      </h1>
       <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
         <Label htmlFor="email">Email</Label>
-        <Input name="email" placeholder="you@example.com" required />
+        <Input
+          name="email"
+          placeholder="akindejioluwaseun015@gmail.com"
+          required
+        />
         <div className="flex justify-between items-center">
           <Label htmlFor="password">Password</Label>
         </div>
         <Input
           type="password"
           name="password"
-          placeholder="Your password"
+          placeholder="Abc1234#"
           required
         />
         <SubmitButton pendingText="Signing In..." formAction={signInAction}>
